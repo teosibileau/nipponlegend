dd433c5258fec4c12e529e966a81b3189fbeba72
Teofilo Sibileau <teo.sibileau@gmail.com>
2026-05-13 19:31:02 -0300

    Integrate changes so / translate to GH page base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  site: 'https://teosibileau.github.io',
+  base: '/nipponlegend',
+  trailingSlash: 'always',
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
